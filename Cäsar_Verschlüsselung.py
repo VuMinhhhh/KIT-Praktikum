a1ba517100811bc3d9671d6a07494e241f3bb442
VuMinhhhh <123296073+VuMinhhhh@users.noreply.github.com>
2025-04-25 15:21:35 +0200

    Update Cäsar_Verschlüsselung.py

--- Cäsar_Verschlüsselung.py
+++ Cäsar_Verschlüsselung.py
@@ -4,13 +4,14 @@ print("Geben Sie den Schluessel:", end = "")
 Schluessel = int(input())
 Geheimtext = ""
 for i in range (len(Klartext)):
-    zum_ascii = ord(Klartext[i])
-    if Klartext[i] == ' ':
-        Geheimtext += ' '
-    elif 97 <= zum_ascii and zum_ascii <= 122: 
-        Geheimtext += chr((zum_ascii - 97 + Schluessel) % 26  + 97)
-    elif 65 <= zum_ascii and zum_ascii <= 90: 
-        Geheimtext += chr((zum_ascii - 65 + Schluessel) % 26  + 65)
+    ascii = ord(Klartext[i])
+    if 97 <= ascii and ascii <= 122: 
+        Geheimtext += chr((ascii - 97 + Schluessel) % 26  + 97)
+    elif 65 <= ascii and ascii <= 90: 
+        Geheimtext += chr((ascii - 65 + Schluessel) % 26  + 65)
+    else:
+        Geheimtext += Klartext[i]
 print("Hier ist den Geheimtext:", Geheimtext)
 with open('Geheimtext.txt', 'w') as file:
     file.write(Geheimtext)
+
