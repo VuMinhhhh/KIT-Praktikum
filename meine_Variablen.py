54a5114040954f7fe4bf829ad4eee694f76e22b5
VuMinhhhh <123296073+VuMinhhhh@users.noreply.github.com>
2025-05-17 15:45:09 +0200

    Create meine_Variablen.py

---
+++ meine_Variablen.py
@@ -0,0 +1,2 @@
+Zufallsantworten = ["Oh, wirklich", "Interessant ...", "Das kann man so sehen", "Ich verstehe ..."]
+Reaktionsantworten = {"hallo": "Hallo", "bye": "Einen schönen Tag wünsche ich dir. Bis zum nächsten Mal"}
