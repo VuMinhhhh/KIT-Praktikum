de9c0022784646681baf5d20e1d38a0af89d406b
VuMinhhhh <123296073+VuMinhhhh@users.noreply.github.com>
2025-05-21 10:03:57 +0200

    Delete meine_Variablen.py

--- meine_Variablen.py
+++
@@ -1,2 +0,0 @@
-Zufallsantworten = ["Oh, wirklich", "Interessant ...", "Das kann man so sehen", "Ich verstehe ..."]
-Reaktionsantworten = {"hallo": "Hallo", "bye": "Einen schönen Tag wünsche ich dir. Bis zum nächsten Mal"}
